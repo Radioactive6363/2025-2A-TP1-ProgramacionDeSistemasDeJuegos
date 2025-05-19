1ddf05f06e8d64f70eb8d313f7b8020350c050f5
TomasBurgos <tomasburgos2002@outlook.com>
2025-05-19 16:04:43 -0300

    Ajustes de comentarios.

--- Assets/1_ServiceAccess/Scripts/Characters/CharacterService.cs
+++ Assets/1_ServiceAccess/Scripts/Characters/CharacterService.cs
@@ -7,8 +7,9 @@ namespace Excercise1
     {
         /*
          NOTE:
-         Because we want to keep the class non-static, I created an instance 
-         within the class so that it can be accessed by components of the character class.
+         Because it was never specified if the class should be non-static or static, I created an instance 
+         within the class so that it can be accessed by components of the character class. And since it also uses
+         MonoBehaviour and is attached to a GameObject, I cant remove it.
          It could be improved by making the class static, So that we don't have to create an instance of this, 
          but since I don't know if that's what we want to continue using it as an instance from scene to scene, 
          I'll maintain the convention.
